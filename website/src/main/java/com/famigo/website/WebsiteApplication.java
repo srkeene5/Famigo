4101d7d071be610528de9eef7d9d3249d8fc41ba
Nitin Murthy <77122711+nmurthy932@users.noreply.github.com>
2024-02-17 17:35:33 -0500

    tried to create a google cloud database

--- website/src/main/java/com/famigo/website/WebsiteApplication.java
+++ website/src/main/java/com/famigo/website/WebsiteApplication.java
@@ -2,12 +2,18 @@ package com.famigo.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WebsiteApplication {
+    
+    static Profile profile;
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(WebsiteApplication.class, args);
+        Profile profile = context.getBean(Profile.class);
+        profile.createProfile("user", "private", "email", "cain", "abel", "something");
+        profile.printProfile();
+    }
 
 }
