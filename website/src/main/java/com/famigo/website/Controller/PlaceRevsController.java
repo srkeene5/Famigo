8246675cb7bf74fdcee87e61feccfb9c72dd7349
ZackAttack47 <BOOKFANZACK@gmail.com>
2024-03-01 00:54:49 -0500

    Edits to small things such as color of dark mode and new alert system

--- website/src/main/java/com/famigo/website/Controller/PlaceRevsController.java
+++ website/src/main/java/com/famigo/website/Controller/PlaceRevsController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.famigo.website.service.PlaceRevsService;
+import com.famigo.website.Service.PlaceRevsService;
 
 @Controller
 public class PlaceRevsController {
