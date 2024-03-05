810d97e668ddafda7a8db9f907d6692f4ba63ad7
JoeyFoutty <jfoutty@purdue.edu>
2024-03-04 20:27:54 -0500

    Added javaMail springframework

---
+++ website/src/main/java/com/famigo/website/Service/EmailService.java
@@ -0,0 +1,30 @@
+package com.famigo.website.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+      
+        javaMailSender.send(message);
+    }
+}
